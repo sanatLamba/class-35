9f1e719e842334e2dd300d17d2920323e80ac497
sanatLamba <70831283+sanatLamba@users.noreply.github.com>
2020-11-22 11:03:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+
+
+function setup(){
+  database = firebase.database();
+  //console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  
+    drawSprites();
+  
+}
